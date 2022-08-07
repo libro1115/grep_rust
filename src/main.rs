90f2c75162d4d1db87896246d137d851b9138f18
=libro1115 <libro.1115@gmail.com>
2022-08-07 19:51:43 +0900

    処理を分割

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
-use std::{env, process, fs::File, io::prelude::*};
+use std::{env, error::Error, fs::File, io::prelude::*, process};
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    let config = Config::new(&args)
-                            .unwrap_or_else(|err|
-                                {
-                                    println!("Problem parsing arguments: {}",err);
-                                    process::exit(1);
-                                });
-    println!("searching for {}",config.query);
-    println!("In file {}",config.filename);
-    
-    let mut f = File::open(config.filename).expect("file not found");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("search ing for {}", config.query);
+    println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error:{}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    println!("With text:^n{}",contents);
+
+    f.read_to_string(&mut contents)?;
+    println!("With text:\n{}", contents);
+    Ok(())
 }
-struct Config{
-    query:String,
-    filename:String,
+struct Config {
+    query: String,
+    filename: String,
 }
-impl Config{
-    fn new(args:&[String])->Result<Config, &'static str>{
-        if args.len() < 3{
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
             return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename})
+        Ok(Config { query, filename })
     }
 }
