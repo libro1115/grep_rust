bfb26420721f1ce9e14cd57ac6078ef560063a02
=libro1115 <libro.1115@gmail.com>
2022-08-07 15:16:51 +0900

    エラー処理の改善

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::{env, fs::File, io::prelude::*};
+use std::{env, process, fs::File, io::prelude::*};
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let config = Config::new(&args); 
+    let config = Config::new(&args)
+                            .unwrap_or_else(|err|
+                                {
+                                    println!("Problem parsing arguments: {}",err);
+                                    process::exit(1);
+                                });
     println!("searching for {}",config.query);
     println!("In file {}",config.filename);
     
@@ -16,9 +21,12 @@ struct Config{
     filename:String,
 }
 impl Config{
-    fn new(args:&[String])->Config{
+    fn new(args:&[String])->Result<Config, &'static str>{
+        if args.len() < 3{
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename}
+        Ok(Config { query, filename})
     }
 }
