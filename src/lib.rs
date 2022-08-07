5d802b45ab8d6a0c7578905eafbdc5cf0719ee10
=libro1115 <libro.1115@gmail.com>
2022-08-07 21:14:59 +0900

    環境変数と大文字小文字区別なし検索

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs::File, io::prelude::*};
+use std::{env, error::Error, fs::File, io::prelude::*};
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub ignore_case: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -11,7 +12,12 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let ignore_case = env::var("CASE_IGNORE").is_ok();
+        Ok(Config {
+            query,
+            filename,
+            ignore_case,
+        })
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -19,16 +25,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
 
     f.read_to_string(&mut contents)?;
-    for line in search(&config.query, &contents){
-        println!("{}",line);
+
+    let results = if config.ignore_case {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
     }
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query){
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -36,18 +59,28 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
-    fn one_result(){
+    fn one_result() {
         let query = "duct";
         let contents = "/
 Rust:
 safe,fast,productive.
 pick three.";
+        assert_eq!(vec!["safe,fast,productive."], search(query, contents));
+    }
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "/
+Rust:
+safe,fast,productive.
+pick three.
+Trust me.";
         assert_eq!(
-            vec!["safe,fast,productive."],
-            search(query, contents)
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
