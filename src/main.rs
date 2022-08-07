544d49e8ba8e7bf4995bfdc08c8b68731abf90e8
=libro1115 <libro.1115@gmail.com>
2022-08-07 20:04:59 +0900

    libファイルに移動

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs::File, io::prelude::*, process};
+extern crate grep_rust;
+use grep_rust::{run, Config};
+use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -10,28 +12,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error:{}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
     }
 }
