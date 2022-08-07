6e7d5373077330a8c941d95bcd6a1d95e049a32c
=libro1115 <libro.1115@gmail.com>
2022-08-07 14:48:35 +0900

    構造体Configの作成

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::{env, fs::File, io::prelude::*};
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-
-    println!("searching for {}",query);
-    println!("In file {}",filename);
+    let config = Config::new(&args); 
+    println!("searching for {}",config.query);
+    println!("In file {}",config.filename);
     
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
     println!("With text:^n{}",contents);
+}
+struct Config{
+    query:String,
+    filename:String,
+}
+impl Config{
+    fn new(args:&[String])->Config{
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename}
+    }
 }
