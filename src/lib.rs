0cb8a901fe2abada9d1c90af8e48b8023ee996a4
=libro1115 <libro.1115@gmail.com>
2022-08-08 16:36:18 +0900

    イテレータによる修正

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,16 @@ pub struct Config {
     pub ignore_case: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args)-> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next(){
+            Some(arg) =>arg,
+            None => return Err("Didn't get a puery string"),
+        };
+        let filename = match args.next(){
+            Some(arg)=>arg,
+            None => return Err("Didn't get a file name"),
+        };
         let ignore_case = env::var("CASE_IGNORE").is_ok();
         Ok(Config {
             query,
@@ -38,13 +42,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
